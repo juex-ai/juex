ce4796d8ab14e00ada1df8932391b1d34dba7478
He High <celalnsa@gmail.com>
2026-07-18 00:47:49 +0800

    test: handle Windows config permissions

--- internal/config/workspace_config_test.go
+++ internal/config/workspace_config_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 )
 
@@ -87,7 +88,7 @@ providers:
 	if err != nil {
 		t.Fatal(err)
 	}
-	if info.Mode().Perm() != 0o600 {
+	if runtime.GOOS != "windows" && info.Mode().Perm() != 0o600 {
 		t.Fatalf("mode = %o, want 600", info.Mode().Perm())
 	}
 }
